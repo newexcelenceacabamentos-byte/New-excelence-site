be2382438cc2891d1b16cfd7a13df8f332b36b09
newexcelenceacabamentos-byte <newexcelenceacabamentos@gmail.com>
2026-07-24 13:05:54 -0300

    Add smooth scrolling for menu links

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// New Excelence Acabamentos
 
+console.log("Site carregado com sucesso!");
+
+// Rolagem suave dos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const destino = document.querySelector(this.getAttribute("href"));
+
+    if (destino) {
+      destino.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
